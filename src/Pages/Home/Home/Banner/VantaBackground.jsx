00262fe84c5e70fcda7fc8c5c53ab11503c7939e
Shakib0976 <web.shakib09@gmail.com>
2025-07-29 19:20:25 +0600

    all task complete

--- src/Pages/Home/Home/Banner/VantaBackground.jsx
+++ src/Pages/Home/Home/Banner/VantaBackground.jsx
@@ -1,34 +1,8 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useRef} from "react";
 
 const VantaNet = ({ children, height = "100vh" }) => {
     const vantaRef = useRef(null);
-    const [vantaEffect, setVantaEffect] = useState(null);
-
-    useEffect(() => {
-        if (!vantaEffect && window.VANTA) {
-            setVantaEffect(
-                window.VANTA.NET({
-                    el: vantaRef.current,
-                    mouseControls: true,
-                    touchControls: true,
-                    gyroControls: false,
-                    minHeight: 200.0,
-                    minWidth: 200.0,
-                    scale: 1.0,
-                    scaleMobile: 1.0,
-                    color: 0xb854,
-                    backgroundColor: 0x29 ,// background
-                    points: 10.0,             // number of points
-                    maxDistance: 20.0         // spacing between them
-                })
-            );
-        }
-
-        return () => {
-            if (vantaEffect) vantaEffect.destroy();
-        };
-    }, [vantaEffect]);
-
+  
     return (
         <div
             ref={vantaRef}
