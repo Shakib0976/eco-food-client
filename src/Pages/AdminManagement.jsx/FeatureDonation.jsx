7ecb330f0277f2a32e8e202e7b527e6d8ec5b2c3
Shakib0976 <web.shakib09@gmail.com>
2025-08-08 18:33:34 +0600

    Initial commit from old project

--- src/Pages/AdminManagement.jsx/FeatureDonation.jsx
+++ src/Pages/AdminManagement.jsx/FeatureDonation.jsx
@@ -30,6 +30,12 @@ const FeatureDonation = () => {
                 title: 'featured added Successful!',
             });
         }
+        else {
+            await Swal.fire({
+                icon: 'error',
+                title: 'featured added Alreade added',
+            });
+        }
     }
     return (
         <div className='my-10 w-11/12 mx-auto'>
--- src/Pages/Forbidden.jsx
+++ src/Pages/Forbidden.jsx
@@ -8,10 +8,9 @@ const Forbidden = () => {
             <div className="max-w-md w-full p-8 bg-white dark:bg-gray-800 rounded-lg shadow-xl">
                 {/* Replace with a real illustration path or an icon library */}
                 <img src="https://i.ibb.co/wNDm6Z4B/396851-PCPXUF-274.jpg" alt="Forbidden Illustration" className="w-48 h-48 mx-auto mb-6 opacity-75" />
-                <h1 className="text-5xl font-extrabold text-red-600 dark:text-red-500 mb-4">Access Denied</h1>
+                <h1 className="text-5xl font-extrabold text-red-600 dark:text-red-500 mb-4">Access Denied   /  please clean your cookie token</h1>
                 <p className="text-lg text-gray-700 dark:text-gray-300 mb-6">
-                    It seems you don't have the necessary permissions to view this page.
-                    If you believe this is an error, please reach out to the site administrator.
+                   Clean you token with cookei 
                 </p>
                 <div className="flex flex-col sm:flex-row justify-center gap-4">
                     <Link to="/" className="btn btn-primary btn-lg">
